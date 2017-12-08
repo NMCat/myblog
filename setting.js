afcf23f7e70c7af2c869af130bcc90b16d611c53
cat <947192304@qq.com>
2017-12-08 12:11:08 +0800

    first commit

---
+++ setting.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Administrator on 2017/12/8.
+ */
+//数据库配置信息
+module.exports = {
+    cookieSecret:'cat',
+    db:'blog', //数据库的名称
+    host:'localhost',
+    port:27017 //数据库的端口号
+}
+//我们把数据库的配置信息写在这里，是为了在连接数据库的时候，一旦
+//数据库的地址或者是名称或者是端口号发生变化的时候，我们只需要改这里就可以了。
